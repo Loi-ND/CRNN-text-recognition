1b8949f37fdfd053e9488a68cfac99e0c1caf213
Nguyen-Duy-Loi <loinguyenduy12a1@gmail.com>
2025-04-14 00:25:31 +0700

    14/4

---
+++ dataset.py
@@ -0,0 +1,34 @@
+from torch.utils.data import Dataset
+from typing import List, Tuple
+import torch
+from torchvision.transforms import Normalize, Resize, Compose, ToTensor
+from PIL import Image
+
+class CustomDataset(Dataset):
+    def __init__(self, 
+                 image_paths: List[str], 
+                 targets: List[torch.Tensor],
+                 image_shape: Tuple[int, int]):
+        super().__init__()
+        self.image_paths = image_paths
+        self.targets = targets
+        mean = [0.485]
+        std = [0.225]
+        self.transform = Compose(
+            [
+                Resize(image_shape),
+                Normalize(mean=mean, std=std),
+                ToTensor()
+            ]
+        )
+    
+    def __len__(self):
+        return len(self.image_paths)
+    
+    def __getitem__(self, index):
+        image_path = self.image_paths[index]
+        image = Image.open(image_path).convert('L')
+        image = self.transform(image)
+        target = self.targets[index]
+
+        return image, target
